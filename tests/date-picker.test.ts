b99ecca340a6ec72ea9c89d0e69f57e71406a53c
Ansko <carhatt.xm@gmail.com>
2025-01-26 01:04:16 +0200

    save

--- tests/date-picker.test.ts
+++ tests/date-picker.test.ts
@@ -2,61 +2,57 @@ import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
-    
+
 })
-  test('tables', async({page})=>{
-        await page.getByText('Tables & Data').click()
-        await page.getByText('Smart Table').click()
+test('date-pickers', async ({ page }) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
 
-        //метч рядка по унікальному значенню в ньому
-       const targetRow = page.getByRole('row', {name: 'twitter@outlook.com'}) //спеціальна роль під рядок в таблиці
-       await targetRow.locator('.nb-edit').click();
+    const calendarInputField = page.getByPlaceholder('Form Picker')
+    await calendarInputField.click(); //відкриває календар для вибору дати
 
-      await page.locator('input-editor').getByPlaceholder('Age').clear // placeholder="Age" так виглядає плейсхолдер в хтмл
-      await page.locator('input-editor').getByPlaceholder('Age').fill('11') // placeholder="Age" так виглядає плейсхолдер в хтмл
-      await page.locator('nb-checkmark').click()
+    await page.locator('[class = "day-cell ng-star-inserted"]').getByText('24').click();
+    await expect(calendarInputField).toHaveValue('Jan 24, 2025')
 
-      //якщо в рядку є унікальне зничання доприкладу айді, але воно може десь дублюватись ще в таблиці, тоді:
-      //знаходимо рядокм, далі фільтруємо його по лактору, вказуємо індекс який означає стовпчик таблиці і ще раз вказуємо необхідне пошукове значення
-        
-    await page.locator('.ng2-smart-pagination-nav').getByText('2').click();
-    const targetRowById = page.getByRole('row', { name: '11' }).filter({ has: page.locator('td').nth(1).getByText('11') });
-    await targetRowById.locator('.nb-edit').click();
-    await page.locator('input-editor').getByPlaceholder('E-mail').clear();
-    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com');
-    await page.locator('.nb-checkmark').click();
-    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com');
+})
+test('date-picker2', async ({ page }) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
+
+    const calendarInputField = page.getByPlaceholder('Form Picker')
+    await calendarInputField.click(); //відкриває календар для вибору дати
+
+    let date = new Date() // JS object for date and time
+    date.setDate(date.getDate() + 20)
+    
+    const expectedDate = date.getDate().toString(); //getDate() повертає тільки день і обрізає лишнє
+    const expectedMonthShort = date.toLocaleString('En-Us', { month: 'short' }) // переобразуває дату в короткий формат 'Jan'
+    const expectedMonthLong = date.toLocaleString('En-Us', { month: 'long' }) // переобразуває дату в довгий формат 'January'
+    const expectedYear = date.getFullYear().toString()
+
+    console.log(expectedYear)
+    console.log(expectedMonthLong)
+   
+
+    let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+    const dateToassert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `
+    console.log(expectedMonthAndYear)
+
+    while(!calendarMonthAndYear.includes(expectedMonthAndYear)){ //допоки дата і рік на календарі не будуть відповідати необхідній, виконувати умову - переходити на наступний місяць
+        await page.locator('button.next-month').click();
+        calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+        //спочатку ми отримуємо текст який відображаєтсья на календарі, якщо він не дорівнює нашій експектед даті
+        //то необхідно натиснути на кнопку некст. так як після перемикання на кнопку некст в нас змінюється місяць
+         // то викликаємо ще раз зчитування в expectedMonthAndYear  щоб записати нове значення щоб порівнювати вже актуальні дані на наступному витку
+    }
+
+
+
+    await page.locator('[class = "day-cell ng-star-inserted"]').getByText(expectedDate).click();
+    await expect(calendarInputField).toHaveValue(dateToassert)
+
+
 })
 
- test('tables 2', async({page})=>{
-        await page.getByText('Tables & Data').click()
-        await page.getByText('Smart Table').click()
-
-        const ages = ['20', '200', '30', '40'] //тестові дані які потрібні для перевірки в тесті
-
-
-        //цикл в циклі
-        // 1 відповідає щоб клірити комірку та проставляти в ній велю з обєкта age
-        // наступний цицкл збирає велю яке повертає таблиця і порівнює результат рядка з обєктом age
-        
-    
-        for (let age of ages) {
-            await page.locator('input-filter').getByPlaceholder('Age').clear();
-            await page.locator('input-filter').getByPlaceholder('Age').fill(age);
-            await page.waitForTimeout(500) //тест падає тому що таблиця оновлюється повільніше чим плейврайт записує результат 
-            //тому тут спеціально заповільнюємо, щоб дочеатись фільтрації таблиці 
-
-            const ageRows = page.locator('tbody tr') //локаторя який метчить всі рядки всередині тіла таблиці. 
-            
-            for (let row of await ageRows.all()) { //цикл який йде по кожному рядку таблиці завдяки методу all() 
-                const ageCell = await row.locator('td').last().textContent() // на кожному витку цикла отрммуємо значення комірки age/ метод .last() дістає останнє значення локатору
-                if(age == '200'){
-                    expect(await page.getByRole('table').textContent()).toContain('No data found') //перевірка негативного сценарію, коли таблиця не повртає нічого
-                } else {
-                    expect(ageCell).toEqual(age) //перевіряємо що значення в комірці(-ках) таблиці дорівнює нашому значенню з обєкта
-                }
-
-            }
-        }
- })       
 
